0a7b2e47e9ad35c60f2bc70708c6756c1cdb6237
Alparslan Ozden <ahmetalparslanozden@gmail.com>
2022-04-03 13:35:19 +0300

    Initial commit - Object Oriented Programming - 2

---
+++ app/src/main/java/com/alparslan/object_oriented_programming_2/ConstructorMain.kt
@@ -0,0 +1,12 @@
+package com.alparslan.object_oriented_programming_2
+
+fun main(){
+
+    val kisi = Kisiler(2001,"Kastamonu")
+
+    println(kisi.ad)
+    println(kisi.yas)
+    println(kisi.dogumT)
+    println(kisi.dogumY)
+
+}
---
+++ app/src/main/java/com/alparslan/object_oriented_programming_2/Kisiler.kt
@@ -0,0 +1,12 @@
+package com.alparslan.object_oriented_programming_2
+
+class Kisiler(var dogumT:Int, var dogumY:String ) {  //CONSTRUCTOR KULLANIMI VE INIT YAPISI
+    var ad:String = "Alparslan"
+    var yas:Int = 18
+
+    init {
+        println("------------------------------")
+        println("Kişi Nesnesi Oluşturuldu")
+        println("------------------------------")
+    }
+}
