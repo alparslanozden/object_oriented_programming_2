e020772c9ce6dc2ddee296b3466910aef67b9ae3
Alparslan Ozden <ahmetalparslanozden@gmail.com>
2022-04-05 16:13:07 +0300

    Code Refactor - Static Variables

---
+++ app/src/main/java/com/alparslan/object_oriented_programming_2/Asinifi.kt
@@ -0,0 +1,13 @@
+package com.alparslan.object_oriented_programming_2
+
+class Asinifi {
+
+    companion object{
+        var x = 10
+
+    fun metod(){
+        println("Merhaba")
+    }
+
+    }
+}
---
+++ app/src/main/java/com/alparslan/object_oriented_programming_2/AsinifiMain.kt
@@ -0,0 +1,9 @@
+package com.alparslan.object_oriented_programming_2
+
+fun main(){
+
+    println(Asinifi.x)
+    println(Asinifi.metod())
+
+
+}
