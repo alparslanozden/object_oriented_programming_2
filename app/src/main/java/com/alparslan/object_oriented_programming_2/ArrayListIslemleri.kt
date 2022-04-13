8050cb38666824723ab07e87c32caed90062de3f
Alparslan Ozden <ahmetalparslanozden@gmail.com>
2022-04-13 23:59:01 +0300

    Code Refactor - ArrayList operations

---
+++ app/src/main/java/com/alparslan/object_oriented_programming_2/ArrayListIslemleri.kt
@@ -0,0 +1,16 @@
+package com.alparslan.object_oriented_programming_2
+
+fun main(){
+
+    val meyveler = ArrayList<String>()
+
+    meyveler.add("Elma")
+    meyveler.add("Armut")
+    meyveler.add("Portakal")
+    meyveler.add("Kivi")
+    meyveler.add("Mandalina")
+
+    println(meyveler.isEmpty())             // BOŞ MU
+    println(meyveler.count())               // LİSTİN BOYUTU
+    println(meyveler.size)                  // LİSTİN BOYUTU
+}
