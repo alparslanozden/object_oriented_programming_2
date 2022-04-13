887ed2f6fc3411bf3f5542506a88e1e73f72ba20
Alparslan Ozden <ahmetalparslanozden@gmail.com>
2022-04-14 01:10:43 +0300

    Code Refactor - ArrayList Loops

---
+++ app/src/main/java/com/alparslan/object_oriented_programming_2/ArrayListDonguler.kt
@@ -0,0 +1,19 @@
+package com.alparslan.object_oriented_programming_2
+
+fun main(){
+
+    val meyveler = ArrayList<String>()
+
+    meyveler.add("Elma")
+    meyveler.add("Armut")
+    meyveler.add("Portakal")
+    meyveler.add("Kivi")
+    meyveler.add("Mandalina")
+
+    for (meyve in meyveler){
+        println("Sonuç: $meyve")
+    }
+    for ((indeks, meyve) in meyveler.withIndex()){
+        println("Sonuç 2: $indeks : $meyve")
+    }
+}
