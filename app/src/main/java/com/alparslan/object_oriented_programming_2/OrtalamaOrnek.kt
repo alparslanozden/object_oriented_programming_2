5725a396f0b47dea3028602d808c2572c91b9e82
Alparslan Ozden <ahmetalparslanozden@gmail.com>
2022-04-16 18:09:52 +0300

    Code Refactor - ArrayList average calculation

---
+++ app/src/main/java/com/alparslan/object_oriented_programming_2/OrtalamaOrnek.kt
@@ -0,0 +1,22 @@
+package com.alparslan.object_oriented_programming_2
+
+fun main(){
+
+    val sayilar = ArrayList<Int>()
+
+    sayilar.add(30)
+    sayilar.add(40)
+    sayilar.add(60)
+    sayilar.add(90)
+    sayilar.add(110)
+    sayilar.add(300)
+
+    var toplam = 0
+
+    for (s in sayilar){
+        toplam = toplam + s
+    }
+
+    println("Ortalama: ${toplam / sayilar.size }")
+
+}
