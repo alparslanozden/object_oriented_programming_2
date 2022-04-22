38606901605037a823c14110ba21b21ddc6cf512
Alparslan Ozden <ahmetalparslanozden@gmail.com>
2022-04-22 06:44:47 +0300

    Code Refactor - ArrayList Example - Report Card App

--- app/src/main/java/com/alparslan/object_oriented_programming_2/ArrayListKarneUygulamasi.kt
+++ app/src/main/java/com/alparslan/object_oriented_programming_2/ArrayListKarneUygulamasi.kt
@@ -9,16 +9,36 @@ fun main(){
     val dersNotlari = ArrayList<DersNotlar>()
 
     while (true){
+
         println("Dersin Adını Giriniz: ")
         val ders = girdi.next()
+
         println("Ders Notunu Giriniz: ")
         val not = girdi.nextInt()
+
         val yeniNot = DersNotlar(ders,not)
+        dersNotlari.add(yeniNot)
 
         println("Çıkmak için(1) - Devam etmek için diğer sayılar: ")
         val cikis = girdi.nextInt()
 
             if (cikis == 1){
+                println("***************")
+                var toplam = 0
+
+                for (dn in dersNotlari){
+                    println("${dn.ders} : ${dn.not}")
+                    toplam = toplam + dn.not
+                }
+                val ortalama = toplam / dersNotlari.size
+
+                println("Ortalama: $ortalama")
+
+                if (ortalama>=50){
+                    println("GEÇTİ")
+                }else{
+                    println("KALDI")
+                }
                 println("Çıkış Yapıldı")
                 break
             }
